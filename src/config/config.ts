7f7a78bf2d491c138febca50de8d37aa27be548a
JonathanDPotter <jonathan.d.potter@outlook.com>
2021-12-04 10:18:10 -0500

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,15 @@
+const config = {
+  firebase: {
+    apiKey: "AIzaSyCGJjqsjqt326UKNT3zs_6RMSmFzfCSyFc",
+    authDomain: "typescript-mern-example.firebaseapp.com",
+    projectId: "typescript-mern-example",
+    storageBucket: "typescript-mern-example.appspot.com",
+    messagingSenderId: "1078044004374",
+    appId: "1:1078044004374:web:d79307c5129f3cefa56537",
+  },
+  server: {
+    url: "http://localhost:1337",
+  },
+};
+
+export default config;
